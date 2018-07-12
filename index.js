3d98b0887e3f7611ee9b64e9953f4772fe843365
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-07-12 15:38:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ function findMinAndRemove(array){
   let min = array[0];
   let minIdx = 0;
   for (let i=0; i<array.length; i++) {
+<<<<<<< HEAD
     if (array[i] < min) {
+=======
+    if (array[i] < array[0]) {
+>>>>>>> b862c6bb444545dd13c2a2502038faf9558504ec
       min = array[i];
       minIdx = i;
     }
   }
+<<<<<<< HEAD
   array.splice(minIdx, 1);
+=======
+  array.splice(minIndex, 1);
+>>>>>>> b862c6bb444545dd13c2a2502038faf9558504ec
   return min;
 }
 
